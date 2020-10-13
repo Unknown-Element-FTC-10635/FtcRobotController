22568e7d63a2fad4f26a23c294d4e4ff0914fba4
Sean Krueger <skrueger2270@gmail.com>
2020-10-13 16:18:53 -0500

    Added successful ring detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RingDetectionEasyOpenCV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RingDetectionEasyOpenCV.java
@@ -6,8 +6,9 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
@@ -24,7 +25,8 @@ public class RingDetectionEasyOpenCV extends LinearOpMode {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
 
-        webcam.setPipeline(new SamplePipeline());
+        CameraPipeline pipeline = new CameraPipeline(1, 4);
+        webcam.setPipeline(pipeline);
 
         webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
@@ -33,22 +35,20 @@ public class RingDetectionEasyOpenCV extends LinearOpMode {
             }
         });
 
-        double rings = countRing(0.5, 1.5);
-
         telemetry.addLine("Waiting for start");
-        telemetry.addData("Rings: ", rings);
         telemetry.update();
 
         waitForStart();
 
         while (opModeIsActive()) {
-            telemetry.addData("Frame Count", webcam.getFrameCount());
-            telemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
-            telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
-            telemetry.addData("Pipeline time ms", webcam.getPipelineTimeMs());
-            telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
-            telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
+            double rings = pipeline.getRingCount();
+
+            telemetry.addData("Rings: ", rings);
+            telemetry.addData("Hue Mean: ", pipeline.getHueMean());
+            telemetry.addData("Submat Mean", pipeline.getInputMean());
             telemetry.update();
+
+            pipeline.reset();
 
             if (gamepad1.a) {
                 webcam.stopStreaming();
@@ -60,56 +60,70 @@ public class RingDetectionEasyOpenCV extends LinearOpMode {
 
     }
 
-    private double countRing(double oneRingThreshold, double fourRingThreshold) {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        Imgcodecs imageCodecs = new Imgcodecs();
-        String importPath = "./ProcessFrame";
+    class CameraPipeline extends OpenCvPipeline {
+        private double oneRingThreshold = 0.5;
+        private double fourRingThreshold = 1.5;
+        private double hueMean = 0;
+        private int frames = 0;
+        private Scalar inputMean;
+        Rect target = new Rect(new Point(430, 0), new Point(480, 50));
 
-        Mat ringFrame = imageCodecs.imread(importPath);
-
-        Scalar mean = Core.mean(ringFrame);
-        double val = mean.val[0];
-
-        if (val > fourRingThreshold) {
-            return 4;
-        } else if (val > oneRingThreshold) {
-            return 1;
-        } else {
-            return 0;
+        public CameraPipeline() {
         }
 
-    }
-
-    class SamplePipeline extends OpenCvPipeline {
-        boolean viewportPaused;
+        public CameraPipeline(double oneRingThreshold, double fourRingThreshold) {
+            this.oneRingThreshold = oneRingThreshold;
+            this.fourRingThreshold = fourRingThreshold;
+        }
 
         @Override
         public Mat processFrame(Mat input) {
-            System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-            Imgcodecs imageCodecs = new Imgcodecs();
-            String exportPath = "./ProcessFrame";
+            Mat mask = new Mat();
+            Imgproc.cvtColor(input, mask, Imgproc.COLOR_RGB2HSV);
 
-            Imgproc.cvtColor(input, input, Imgproc.COLOR_RGB2BGR);
-            Imgproc.cvtColor(input, input, Imgproc.COLOR_BGR2HSV);
+            if (webcam.getFrameCount() == 10) {
+                Mat submat = input.submat(target);
 
-            Core.inRange(input, new Scalar(5, 50, 50), new Scalar(15, 255, 255), input);
-
-            if (webcam.getFrameCount() == 1) {
-                imageCodecs.imwrite(exportPath, input);
+                inputMean = Core.mean(submat);
             }
 
-            return input;
+            Core.inRange(mask, new Scalar(5, 50, 50), new Scalar(22, 255, 255), mask);
+
+            hueMean = hueMean + processRing(mask);
+            frames++;
+
+            return mask;
         }
 
-        @Override
-        public void onViewportTapped() {
-            viewportPaused = !viewportPaused;
+        private double processRing(Mat frame) {
+            Scalar mean = Core.mean(frame);
+            return mean.val[0];
+        }
 
-            if (viewportPaused) {
-                webcam.pauseViewport();
+        public int getRingCount() {
+            double val = hueMean / frames;
+
+            if (val > fourRingThreshold) {
+                return 4;
+            } else if (val > oneRingThreshold) {
+                return 1;
             } else {
-                webcam.resumeViewport();
+                return 0;
             }
+
+        }
+
+        public double getHueMean() {
+            return hueMean / frames;
+        }
+
+        public void reset() {
+            hueMean = 0;
+            frames = 0;
+        }
+
+        public Scalar getInputMean() {
+            return inputMean;
         }
     }
 
