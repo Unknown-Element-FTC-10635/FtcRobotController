8cb516c7adc7177c363fa1c0b5245b3d341755b0
Sean Krueger <skrueger2270@gmail.com>
2020-10-17 15:27:12 -0500

    Added test folder for tests Added OpenCV jar (windows) for testing without EasyOpenCV

---
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/HighGoalAimAssistTest.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.junit.Test;
+import org.opencv.core.Core;
+
+public class HighGoalAimAssistTest {
+    @Test
+    public void imageContours() {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        String importFile = "./src/test/resources/FourRings.jpeg";
+        String exportFile = "./build/Contours.jpeg";
+
+        HighGoalAimAssist aimAssist = new HighGoalAimAssist();
+        aimAssist.createContours(importFile, exportFile);
+    }
+}
