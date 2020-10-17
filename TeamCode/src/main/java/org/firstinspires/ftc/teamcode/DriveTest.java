50f36747a844ddbb9ca677e706f9ea362484dc5c
Ellie <elliek2270@icloud.com>
2020-10-17 14:24:52 -0500

    Add EllieTeleOpMode and DriveTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTest.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.openftc.revextensions2.ExpansionHubMotor;
+
+@TeleOp
+public class DriveTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        ExpansionHubMotor leftFront, leftRear, rightRear, rightFront;
+        leftFront = hardwareMap.get(ExpansionHubMotor.class, "FrontLeft");
+        leftRear = hardwareMap.get(ExpansionHubMotor.class, "BackLeft");
+        rightRear = hardwareMap.get(ExpansionHubMotor.class, "BackRight");
+        rightFront = hardwareMap.get(ExpansionHubMotor.class, "FrontRight");
+
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            boolean slowMode = true;
+
+            double forward = -gamepad1.left_stick_x;
+            double right = gamepad1.left_stick_y;
+            double spin = gamepad1.right_stick_x;
+
+            leftFront.setPower(forward - right - spin);
+            leftRear.setPower(forward - right + spin);
+            rightRear.setPower(forward + right - spin);
+            rightFront.setPower(forward + right + spin);
+
+            telemetry.update();
+        }
+    }
+
+}
+
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EllieTeleOpMode.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "Driver control")
+public class EllieTeleOpMode extends OpMode {
+
+    private DcMotor frontLeftDC;
+    private DcMotor backLeftDC;
+    private DcMotor frontRightDC;
+    private DcMotor backRightDC;
+
+    @Override
+    public void init() {
+        frontLeftDC = hardwareMap.get(DcMotor.class, "Front_Left");
+        frontRightDC = hardwareMap.get(DcMotor.class, "Front_Right");
+        backLeftDC = hardwareMap.get(DcMotor.class, "Back_Left");
+        backRightDC = hardwareMap.get(DcMotor.class, "Back_Right");
+
+        frontLeftDC.setDirection(DcMotor.Direction.FORWARD);
+        backLeftDC.setDirection(DcMotor.Direction.FORWARD);
+        frontRightDC.setDirection(DcMotor.Direction.REVERSE);
+        backRightDC.setDirection(DcMotor.Direction.REVERSE);
+
+
+        telemetry.addData("Init", "Complete");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        frontLeftDC.setPower(gamepad1.left_stick_y + gamepad1.left_stick_x);
+        backLeftDC.setPower(gamepad1.left_stick_y - gamepad1.left_stick_x);
+        frontRightDC.setPower(gamepad1.right_stick_y - gamepad1.right_stick_x);
+        backRightDC.setPower(gamepad1.right_stick_y + gamepad1.right_stick_x);
+
+        telemetry.addData("Front Left Motor Speed", frontLeftDC.getPower());
+        telemetry.addData("Back Left Motor Speed", backLeftDC.getPower());
+        telemetry.addData("Front Right Motor Speed", frontRightDC.getPower());
+        telemetry.addData("Back Right Motor Speed", backRightDC.getPower());
+        telemetry.update();
+
+    }
+}
